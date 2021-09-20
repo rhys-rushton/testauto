46771a1921f00fad7791ba7a2436e2882bb3fa0a
Rhys Rushton <rhys.rushton@gmail.com>
2021-09-20 10:29:55 +1000

    updates

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 #import the user class object
 from classes import userClass
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+import time
 
-#main function that will do everything
-def main():
-    em = input("enter email please ")
-    pw = input("enter pword")
-    user = userClass.User(em, pw)
-    print(user.email)
 
-main()
+
+
+#create the user class
+#import user Class Object 
+
+em = input("enter email please ")
+pw = input("enter pword")
+user = userClass.User(em, pw)
+
+    
+def openBrowser():
+    driver = webdriver.Chrome()
+    driver.get("https://app.respiratoryclinic.com.au/login")
+    # username id="inputUsername"
+    userName = driver.find_element_by_id("inputUsername")
+    userName.clear()
+    userName.send_keys(user.email)
+    time.sleep(5)
+    
+    
+
+openBrowser()
+
+
 
