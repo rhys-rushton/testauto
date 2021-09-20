187fe0bc87230b35120b3b62cca5e67aae2d77d4
Rhys Rushton <rhys.rushton@gmail.com>
2021-09-20 11:15:06 +1000

    login nearly completed

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ https://docs.python.org/3/library/csv.html
 Selenium 
 https://selenium-python.readthedocs.io/installation.html
 
+
+# A rough working outline of how the project will work. 
+The script starts. 
+The script loads the excel data and gets it ready to go. 
+Username information is entered
+The script logs me in 
+the script waits for me to enter authenticator password. 
+The script then logs me in to the main page. 
+The script then commences uploading the data. 
+It checks whether or not the patient is existing. If the patient exists it executes that work flow. 
+If the patient doesn't exist then it executes the other work flow. 
--- main.py
+++ main.py
@@ -15,18 +15,33 @@ pw = input("enter pword")
 user = userClass.User(em, pw)
 
     
-def openBrowser():
+def login():
     driver = webdriver.Chrome()
     driver.get("https://app.respiratoryclinic.com.au/login")
     # username id="inputUsername"
     userName = driver.find_element_by_id("inputUsername")
+    passWord = driver.find_element_by_id("inputPassword")
+    firstSignIn = driver.find_element_by_xpath("//*[@id=\"regular-login\"]/button")
     userName.clear()
+    passWord.clear()
     userName.send_keys(user.email)
+    passWord.send_keys(user.password)
+    firstSignIn.click()
+    #enter google auth code part
+    #select auth input 
+    authInput = driver.find_element_by_xpath("//*[@id=\"two_factor_register_code\"]")
+    #authInput.send_keys("hey")
+    print(bool(authInput.get_attribute("value")))
+    
+
+
+
+
     time.sleep(5)
     
     
 
-openBrowser()
+login()
 
 
 
