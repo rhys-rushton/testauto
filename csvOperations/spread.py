37cf18a35695f4bcf3cace1a35f794e24f28663b
Rhys Rushton <rhys.rushton@gmail.com>
2021-10-08 16:29:32 +1100

    csv done!!!

--- csvOperations/spread.py
+++ csvOperations/spread.py
@@ -1,44 +1,103 @@
 import pandas as pd
+from datetime import datetime
 
-dfRC = pd.read_csv('./REDRC.csv', header=0)
-dfDPS = pd.read_csv('./DSPatients.csv', header =0, encoding = 'cp1252')
-finalData = pd.DataFrame(columns=["FILE_NUMBER","GIVEN_NAME","FAMILY_NAME","HOME_ADDRESS_LINE_1","HOME_ADDRESS_LINE_2","HOME_SUBURB_TOWN","HOME_POSTCODE","DOB","GENDER","TITLE"])
-#unMatched = pd.DataFrame(columns=["FILE_NUMBER"])
-final =pd.DataFrame()
+fields = ['encounter_date', 'encounter_time', 'encounter_id', 'first_name', 'last_name', 'date_of_birth', 'age_at_presentation', 'gender', 'medicare_number', 'indigenous_status', 'address_line1', 'suburb', 'state', 'postcode', 'emergency_contact_name', 'country_of_birth','home_language', 'patient_symptoms', 'usual_medications', 'specimen_collected', 'diagnosis', 'outcome']
+data = pd.read_csv('./data.csv', header=0, encoding = 'cp1252', usecols= fields)
+data_small = data.head()
+data_done = {}
 
-dfRCTest = dfRC.head()
-dfDPSTest = dfDPS.head()
+format = "%d/%m/%Y"
+start_date = input("Please enter the date from which you would like results. Please enter as dd-month-year")
+res = True
+#start_date = "04/08/2021"
+
+try:
+    res = bool(datetime.strptime(start_date, format))
+    print("Date Successfully Entered")
+
+except ValueError:
+    res = False
+    print("Please enter a correct date format")
+
+#new = data[["encounter_date"] == start_date,["encounter_id"]]
+
+#print(((data.head()).transpose()).to_dict())
+## we want to specify a date range. 
+##we only want dates that are greater than the last range that we did. 
+##record last range done. 
+##filter through given dataframe
+##add rows that are in specified date range to a different dataframe. 
+
+def make_dic(inputData, dateCompare):
+    transposed_data = (inputData.transpose()).to_dict()
+    #print(transposed_data)
+    new_key_value = 0
+
+    for key in transposed_data: 
+        if transposed_data[key]['encounter_date'] == dateCompare:
+            
+           
+            data_done[new_key_value] = transposed_data[key]
+            print(data_done[new_key_value])
+            new_key_value += 1
+
+
+#create a dictionary that uses the encounter number as key and date as value 
+##then iterate over all values in dictionary. 
+#all values that are in date range append encounter number to a list. 
+#then use this list as a way to filter through the large dataframe. 
+
+make_dic(data, start_date)
+#print(data_done)
+
+
+
+
+
+
+
+
+
+#dfRC = pd.read_csv('./REDRC.csv', header=0)
+#dfDPS = pd.read_csv('./DSPatients.csv', header =0, encoding = 'cp1252')
+#finalData = pd.DataFrame(columns=["FILE_NUMBER","GIVEN_NAME","FAMILY_NAME","HOME_ADDRESS_LINE_1","HOME_ADDRESS_LINE_2","HOME_SUBURB_TOWN","HOME_POSTCODE","DOB","GENDER","TITLE"])
+
+
+#final =pd.DataFrame()
+
+#dfRCTest = dfRC.head()
+#dfDPSTest = dfDPS.head()
 
 #A function to compare file numbers that are both in the rc and the dps information
-def compareRcDPS (rcNum, dpsNum, rcFull, DPSFull):
-    finalDataFileNumbs = []
-    rcClinic = rcNum.tolist()
-    dpsInfo = dpsNum.tolist()
+#def compareRcDPS (rcNum, dpsNum, rcFull, DPSFull):
+    #finalDataFileNumbs = []
+   # rcClinic = rcNum.tolist()
+   # dpsInfo = dpsNum.tolist()
 
-    for n in rcClinic: 
-        if n in dpsInfo:
+    #for n in rcClinic: 
+        #if n in dpsInfo:
             #print("Match")
-            finalDataFileNumbs.append(n)
-        else: 
+         #   finalDataFileNumbs.append(n)
+      #  else: 
             #print("No")
-            continue
+        #    continue
     
-    fd = pd.DataFrame(finalDataFileNumbs, columns=["FILE_NUMBER"])
+   # fd = pd.DataFrame(finalDataFileNumbs, columns=["FILE_NUMBER"])
 
     #this is the data we are going to upload
     #we are checking that the list of file numbers in the final data column
     #is inside the rc clinic data. 
-    upload = rcFull[rcFull["FILE_NUMBER"].isin(finalDataFileNumbs)] 
-    supload = DPSFull[DPSFull["FILE_NUMBER"].isin(finalDataFileNumbs)]
-    print(upload)
-    print(supload)
+   # upload = rcFull[rcFull["FILE_NUMBER"].isin(finalDataFileNumbs)] 
+   # supload = DPSFull[DPSFull["FILE_NUMBER"].isin(finalDataFileNumbs)]
+   # print(upload)
+    #print(supload)
     
 
     
   
  
         
-compareRcDPS (dfRC["FILE_NUMBER"], dfDPS["FILE_NUMBER"],dfRC, dfDPS[['FILE_NUMBER' , 'GENDER', 'DATE_OF_BIRTH']])
+#compareRcDPS (dfRC["FILE_NUMBER"], dfDPS["FILE_NUMBER"],dfRC, dfDPS[['FILE_NUMBER' , 'GENDER', 'DATE_OF_BIRTH']])
 
 #now we need a function to write the file numbers to the pandas final data data frame. 
 
