6b856e6d0cffdd24a5f601cc088435adf3a91646
Rhys Rushton <rhys.rushton@gmail.com>
2021-10-11 09:54:02 +1100

    integrated generating patient class with selenium automation.

--- main.py
+++ main.py
@@ -4,24 +4,25 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+import time as times
 import pandas as pd
 from csvOperations import spread
 
 
 data_to_use = spread.data_done
+#print(data_to_use)
 
-print(data_to_use[0])
+#print(data_to_use[0])
 
 
 #create the user class
 #import user Class Object 
-#em = input("enter email please ")
-#pw = input("enter pword")
-#user = userClass.User(em, pw)
+em = input("enter email please ")
+pw = input("enter pword")
+user = userClass.User(em, pw)
 
     
-def login():
+def automate(patient_dict, patient_class):
     print('''
     Have your auth ready
     Have your auth ready
@@ -53,12 +54,49 @@ def login():
         return 
     
 
-    time.sleep(10)
+    #times.sleep(10)
+
+    for key in patient_dict:
+        print(key)
+        date = patient_dict[key]['encounter_date']
+        time = patient_dict[key]['encounter_time']
+        ident = patient_dict[key]['encounter_id']
+        first_name = patient_dict[key]['first_name']
+        last_name = patient_dict[key]['last_name']
+        DOB = patient_dict[key]['date_of_birth']
+        age = patient_dict[key]['age_at_presentation']
+        gender = patient_dict[key]['gender']
+        medicare = patient_dict[key]['medicare_number']
+        atsi =  patient_dict[key]['indigenous_status']
+        address = patient_dict[key]['address_line1']
+        suburb = patient_dict[key]['suburb']
+        state = patient_dict[key]['state']
+        postcode = patient_dict[key]['postcode']
+        emergency = patient_dict[key]['emergency_contact_name']
+        birth_country = patient_dict[key]['country_of_birth']
+        language = patient_dict[key]['home_language']
+        symptoms = patient_dict[key]['patient_symptoms']
+        meds = patient_dict[key]['usual_medications']
+        specimen = patient_dict[key]['specimen_collected']
+        diagnosis = patient_dict[key]['diagnosis']
+        outcome = patient_dict[key]['outcome']
+        patient = patient_class.Patient(date,time,ident,first_name, last_name, DOB, age, gender, medicare, atsi, address, suburb, state, postcode, emergency, birth_country, language, symptoms, meds,specimen, diagnosis, outcome )
+        #print(patient.first_name+ " " + patient.last_name + " " + gender + f"{ident}")
+        
+       
     
 
     
-#LOGIN FUNCTION CURRENTLY CHILLING
-#login()
+automate(data_to_use, patientClass)
+
+
+
+
+
+
+
+
+
 
 
 def make_patient (patient_dict, patient_class):
@@ -87,7 +125,8 @@ def make_patient (patient_dict, patient_class):
         diagnosis = patient_dict[key]['diagnosis']
         outcome = patient_dict[key]['outcome']
         patient = patient_class.Patient(date,time,ident,first_name, last_name, DOB, age, gender, medicare, atsi, address, suburb, state, postcode, emergency, birth_country, language, symptoms, meds,specimen, diagnosis, outcome )
-        print(patient.first_name + patient.last_name + gender)
+        print(patient.first_name+ " " + patient.last_name + " " + gender)
+        print(key)
 
     
 
@@ -96,4 +135,4 @@ def make_patient (patient_dict, patient_class):
 #then do a series of steps where we go through the website -> patient is either existing or not. 
 ##then we want to record the outcome of these steps 
 
-make_patient(data_to_use, patientClass)
+#make_patient(data_to_use, patientClass)
